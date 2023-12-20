9d4619d71ee6af9415430694beace48bc9359692
wendigo <saadmansakib275@gmail.com>
2023-12-20 20:35:50 +0600

    new test added

--- src/main/java/org/example/GeneralStore.java
+++ src/main/java/org/example/GeneralStore.java
@@ -30,22 +30,51 @@ public class GeneralStore extends Main{
         Assert.assertEquals(msg,"Please enter your name");
     }
 
+    //adding a product and checking out cart and the webviewer
     @Test
     public void shopping() throws InterruptedException{
         create_account();
-
         driver.findElement(AppiumBy.androidUIAutomator(
                 "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"))"));
         driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+
+        Thread.sleep(2000);
+        Set <String> context_names=driver.getContextHandles();
+
+        for(String context_name:context_names){
+            System.out.println(context_name);
+        }
         driver.context("WEBVIEW_com.androidsample.generalstore");
-        Thread.sleep(6000l);
+        Thread.sleep(6000);
         driver.findElement(By.name("q")).sendKeys("UIU");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        Thread.sleep(2000l);
-        driver.pressKey(new KeyEvent(AndroidKey.BACK));
-        Thread.sleep(2000l);
-        driver.context("NATIVE_APP");
+        Thread.sleep(3000);
+    }
+
+    //adding multiple products
+    @Test
+    public void shopping_multi() throws InterruptedException{
+        create_account();
+        driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"))"));
+        driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+        driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector()).scrollIntoView(text(\"PG 3\"))"));
+        driver.findElement(By.id("com.androidsample.generalstore:id/productAddCart")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+
+        Thread.sleep(2000);
+    }
+
+    //null cart error msg
+    @Test
+    public void cart_error_msg(){
+        create_account();
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        String msg=driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+        Assert.assertEquals(msg,"Please add some product at first");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,12 @@ public class Main{
     public void configure_appium() throws MalformedURLException{
         //must turn on appium server using the CLI and import SLF4J-nop libs
         UiAutomator2Options options=new UiAutomator2Options();
-        options.setDeviceName("VM_Phone");
-        options.setApp(System.getProperty("user.dir")+"\\src\\main\\java\\General-Store.apk"); //the full path is required. use user.dir
+        options.setDeviceName("VM_Phone"); //using android studio emulator
+
+//        options.setApp(System.getProperty("user.dir")+"\\src\\main\\java\\app\\ApiDemos-debug.apk"); //the full path is required. use user.dir
+        options.setApp(System.getProperty("user.dir")+"\\src\\main\\java\\app\\General-Store.apk"); //the full path is required. use user.dir
+        options.setChromedriverExecutable(System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+
         driver=new AndroidDriver(new URL("http://127.0.0.1:4723/"),options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
